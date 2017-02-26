0fc9d673f3660847dc32060ba395e3284ab110d1
KMIntelligentSystems <kim.mcclymont@westnet.com.au>
2017-02-27 10:40:44 +1100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require("gulp");
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+const tsc = require("gulp-typescript");
+const sourcemaps = require('gulp-sourcemaps');
+const tsProject = tsc.createProject("tsconfig.json");
+
+var paths = {
+    pages: ['src/*.html']
+};
+
+gulp.task("copy-html", function () {
+    return gulp.src(paths.pages)
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["copy-html"], function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("resources", () => {
+    return gulp.src(["src/**/*", "!**/*.ts"])
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("compile", () => {
+    var tsResult = gulp.src("src/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tsProject));
+    return tsResult.js
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("build"));
+});
+
+gulp.task("libs", () => {
+    return gulp.src([
+            'core-js/client/shim.min.js',
+            'systemjs/dist/system-polyfills.js',
+            'systemjs/dist/system.src.js',
+            'reflect-metadata/Reflect.js',
+            'rxjs/**',
+            'zone.js/dist/**',
+            '@angular/**'
+        ], {cwd: "node_modules/**"}) /* Glob required here. */
+        .pipe(gulp.dest("build/lib"));
+});
+
+gulp.task("build", ['compile','resources', 'libs'], () => {
+    console.log("Building the project ...");
+});
