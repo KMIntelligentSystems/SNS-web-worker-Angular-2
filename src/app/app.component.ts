08c9e5d9c0f7cb35130190f351e31f3cff1124a0
KMIntelligentSystems <kim.mcclymont@westnet.com.au>
2017-02-27 10:55:52 +1100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,6 @@ import { Directive, ElementRef, Input } from '@angular/core';
 
 
 import { NotificationService } from './app.notification.service';
-import {NotificationController} from './app.notification.controller';
-
-let p: Promise<string> = new Promise (
-   (resolve: (str: string)=>void, reject: (str: string)=>void) =>{
-       
-       
-       var notificationService = new NotificationService();
-          notificationService.startWork('start').then(function(response) {
-    // complete
-        }, function(error) {
-    // error
-        }, function(response) {
-    // notify (here you receive intermittent responses from worker)
-        var res = JSON.parse(response);
-        var msg = JSON.parse(res.msg)
-    
-        resolve(msg.Message);
-        function test() : string{return "tet"}
-        return test;
-      }); 
-     }
- );
 
   
 
@@ -41,69 +19,32 @@ export class AppComponent {
 
   constructor()
   {
-      
-   /*this.promise = p.then();
-    p.then((st) => {
-        this.message = st;
-    });*/
-  }
+     }
 
 
   getMessagesClicked(event) 
   {
       alert('waiting for message')
-    var pp  = new Promise<string> (
-   (resolve: (str: string)=>void, reject: (str: string)=>void) =>{
-       
-       
-       var notificationService = new NotificationService();
+       var p  = new Promise<string> (
+      (resolve: (str: string)=>void, reject: (str: string)=>void) =>{
+         var notificationService = new NotificationService();
           notificationService.startWork('start').then(function(response) {
-    // complete
+         // complete
         }, function(error) {
-    // error
+         // error
         }, function(response) {
-    // notify (here you receive intermittent responses from worker)
-        var res = JSON.parse(response);
-        var msg = JSON.parse(res.msg)
+         // notify (here you receive intermittent responses from worker)
+            var res = JSON.parse(response);
+            var msg = JSON.parse(res.msg);
     
-        resolve(msg.Message);
-    
-      }); 
-    }
-   );
- this.promise = pp.then();
-}
+            resolve(msg.Message);
+         }); 
+      }
+    );
+    this.promise = p.then();
+   }
 }
 
     
    
- /*  notificationService: NotificationService = new NotificationService();
-  public getMessages(): void {
-      
-  this.notificationService.startWork('start').then(function(response) {
-    // complete
-        }, function(error) {
-    // error
-        }, function(response) {
-    // notify (here you receive intermittent responses from worker)
-        var res = JSON.parse(response);
-        var msg = JSON.parse(res.msg)
-       // console.log(msg.Message)
-       // console.log(msg)
-        //test.appComponent.message = msg.Message;
-        
-        var promise = new Promise<string>(function(resolve, reject) {
-                resolve(msg.Message)
-        });
-
-        //emitter.emit(msg.Message);
-      }); 
-      
-
-//return aMessage;
-   } 
-
-public myCallback: {(msg: string): string;};*/
  
-
-
